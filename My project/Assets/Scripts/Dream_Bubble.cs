0f8fe4c16aa2642c43b5b755fd6963d78f1d5714
Timothy Yap <timothyyap1106@gmail.com>
2025-01-24 23:53:26 -0800

    Setup + Making classes

---
+++ My project/Assets/Scripts/Dream_Bubble.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+public class Dream_Bubble
+{
+    public int Bubble_type;
+    public bool is_popped;
+    public float speed;
+
+    public Dream_Bubble()
+    {
+        this.Bubble_type = 0;
+        this.is_popped = false;
+        this.speed = 2.0f;
+    }
+}
---
+++ My project/Assets/Scripts/Teacher.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class Teacher
+{
+    public int num_popped_bubbles;
+    // Tool_held - tool
+    // Tool_inventory - array of tools
+
+    public Teacher() {
+        this.num_popped_bubbles = 0;
+
+    }
+
+    public void Attack ()
+    {
+        // is attacking == true?
+    }
+}
